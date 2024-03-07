a59dd2d938547a2a283781fde0fb7acd866cda28
Ashuneko <ashleehutch22@gmail.com>
2024-03-07 13:48:09 +1030

    - when a pearl hits the border, the player will be teleported to the floor

--- src/main/java/kiul/kiulduelsv2/arena/ArenaListeners.java
+++ src/main/java/kiul/kiulduelsv2/arena/ArenaListeners.java
@@ -2,11 +2,22 @@ package kiul.kiulduelsv2.arena;
 
 import kiul.kiulduelsv2.duel.DuelListeners;
 import kiul.kiulduelsv2.duel.DuelMethods;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.projectiles.ProjectileSource;
 
 public class ArenaListeners implements Listener {
 
@@ -21,5 +32,16 @@ public class ArenaListeners implements Listener {
         }
     }
 
-
-}
+    @EventHandler
+    public void pearlDrop (ProjectileHitEvent e) {
+        Projectile p = e.getEntity();
+            if (e.getHitBlock() != null && e.getHitBlock().getType() == Material.BEDROCK) {
+                e.setCancelled(true);
+                double xLoc = p.getLocation().getX();
+                double zLoc = p.getLocation().getZ();
+                double yLoc = DuelMethods.getHighestBlockBelow(199,p.getLocation()).getY();
+                Location newloc = new Location(e.getHitBlock().getLocation().getWorld(),xLoc,yLoc,zLoc);
+                p.teleport(newloc);
+            }
+        }
+    }
